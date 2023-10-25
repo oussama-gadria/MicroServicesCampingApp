ac8714e8f26f9b1539be4460d93af71b1464a5ba
MariemKhadhraoui <117642592+MariemKhadhraoui@users.noreply.github.com>
2023-10-26 01:56:26 +0200

    Avis

---
+++ AvisCamping/src/test/java/tn/esprit/AvisCamping/AvisCampingApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.AvisCamping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisCampingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
