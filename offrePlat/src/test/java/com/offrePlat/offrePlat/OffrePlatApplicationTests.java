17ff331dabaacf4712922a4c050f27db9dea84a7
nourelhoudamakni <nourelhouda.makni@esprit.tn>
2023-10-26 00:48:06 +0100

    creation microservice offre de plat

---
+++ offrePlat/src/test/java/com/offrePlat/offrePlat/OffrePlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offrePlat.offrePlat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffrePlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
